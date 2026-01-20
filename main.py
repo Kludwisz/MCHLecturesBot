821bc0b64c22b1e0b67eb5c0d023420deb99bb9e
Kludwisz <kludwisz@gmail.com>
2026-01-20 12:23:24 +0100

    add calendar test

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
 from calendar import Calendar, Lecture
+from dotenv import dotenv_values
+
+config = dotenv_values(".env")
+
+cal = Calendar(
+    config["ACCOUNT_EMAIL"],
+    config["SERVICE_ACCOUNT_SECRET"]
+)
