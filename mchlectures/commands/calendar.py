209aa99761b25c493214564ddbce38658c712cdc
Kludwisz <kludwisz@gmail.com>
2026-01-22 22:50:31 +0100

    add upcoming lectures command

--- mchlectures/commands/calendar.py
+++ mchlectures/commands/calendar.py
@@ -1,7 +1,11 @@
 from mchlectures.gcalendar.service import CalendarService
+from mchlectures.commands.util.bot_errors import *
+from mchlectures.gcalendar.dateutils import month_name_short
 
 import discord
+import arrow
 from enum import Enum
+from textwrap import dedent
 
 
 class CalendarScope(Enum):
@@ -18,6 +22,7 @@ class Calendar(discord.Cog):
 
     @discord.slash_command(name="calendar", description="Renders a selected view of the lecture calendar")
     @discord.option(name="scope", default=CalendarScope.WEEK, type=CalendarScope)
+    @discord.option(name="query", optional=True)
     async def calendar(self, ctx: discord.ApplicationContext, scope: CalendarScope):
         await self.calendar_service.render_calendar_to_file(scope=scope.value, filename="calendar.png")
         
@@ -26,6 +31,28 @@ class Calendar(discord.Cog):
         embed.set_image(url="attachment://calendar.png")
         
         await ctx.respond(embed=embed, file=file)
+
+    @discord.slash_command(name="upcoming_lectures", description="Returns a list of N upcoming lectures")
+    @discord.option(name="limit", default=1, type=int)
+    async def upcoming_lectures(self, ctx: discord.ApplicationContext, limit: int):
+        try:
+            lectures = await self.calendar_service.get_upcoming_lectures(limit)
+            list_embed = discord.Embed(title="Upcoming lectures", color=discord.Color.og_blurple())
+            for lecture in lectures:
+                end = lecture.start_time.shift(minutes=lecture.duration_minutes)
+                lecture_now = "RIGHT NOW! " if arrow.now().is_between(lecture.start_time.shift(days=-7), end) else ""
+                date_text = f"{lecture_now}<t:{int(lecture.start_time.timestamp())}:F>"
+                lecturer = await ctx.interaction.client.fetch_user(414097996956041236)
+                text_contents = dedent(f"""
+                                        > "{lecture.title}"
+                                        > {lecture.duration_minutes} minute lecture by {lecturer.display_name}
+                                        """)
+                
+                list_embed.add_field(name=date_text, value=text_contents, inline=False)
+            await ctx.respond(embed=list_embed)
+
+        except ValueError:
+            await ctx.respond(embed=invalid_arg_error(message=f"limit={limit}"))
 
 
 def setup(bot: discord.Bot):
---
+++ mchlectures/commands/util/bot_errors.py
@@ -0,0 +1,9 @@
+import discord
+
+
+def error(title: str = "Error", message: str = "Something went wrong, and the devs didn't expect it. DM Kris on Discord") -> discord.Embed:
+    return discord.Embed(title=title, description=message)
+
+
+def invalid_arg_error(message: str) -> discord.Embed:
+    return discord.Embed(title="Error", description=f"Invalid command argument: {message}")
